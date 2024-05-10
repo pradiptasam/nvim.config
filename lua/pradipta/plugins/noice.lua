c12d633ce1ae400be103079427a3cb676eb6b51f
Pradipta Samanta <samanta@dkrz.de>
2024-05-10 16:35:00 +0200

    new: added the noice plugin

---
+++ lua/pradipta/plugins/noice.lua
@@ -0,0 +1,25 @@
+return{
+    {
+    "folke/noice.nvim",
+    lazy = false,
+    config = function()
+      require("noice").setup({
+        -- add any options here
+        -- routes = {
+        --   {
+        --     view = "notify",
+        --     filter = { event = "msg_showmode" },
+        --   },
+        -- },
+      })
+    end,
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
+}

389f2ae74480715e7d1c6af79065e3c54cc1949b
Pradipta Samanta <samanta@dkrz.de>
2024-05-14 13:08:06 +0200

    change: added harpoon as an extension to telescope and created new keymaps

--- lua/pradipta/plugins/telescope.lua
+++ lua/pradipta/plugins/telescope.lua
@@ -38,6 +38,7 @@ return {
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("harpoon")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
@@ -47,5 +48,7 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>fm", "<cmd>Telescope resume<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fh", "<cmd>Telescope harpoon marks<cr>", { desc = "Find string under cursor in cwd" })
   end,
 }

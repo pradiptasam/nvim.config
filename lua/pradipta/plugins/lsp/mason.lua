3083cf2f4544396557372c1752a8c4de7c87a960
Pradipta Samanta <samanta@dkrz.de>
2024-05-24 22:18:22 +0200

    Modified mason and treesitter for levante

--- lua/pradipta/plugins/lsp/mason.lua
+++ lua/pradipta/plugins/lsp/mason.lua
@@ -27,28 +27,29 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
+        -- "fortls",
+        -- "tsserver",
+        -- "html",
+        -- "cssls",
+        -- "tailwindcss",
+        -- "svelte",
+        -- "lua_ls",
+        -- "graphql",
+        -- "emmet_ls",
+        -- "prismals",
+        -- "pyright",
       },
     })
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
-      },
-    })
+    -- mason_tool_installer.setup({
+    --   ensure_installed = {
+    --     "prettier", -- prettier formatter
+    --     "stylua", -- lua formatter
+    --     "isort", -- python formatter
+    --     "black", -- python formatter
+    --     "pylint",
+    --     "eslint_d",
+    --   },
+    -- })
   end,
 }

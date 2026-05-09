0b8b3e4aacde9bd05484913b83a3d08f600fd988
Pradipta Samanta <samanta@dkrz.de>
2026-05-09 10:27:03 +0200

    updated the copilot files

--- lua/user/plugins/copilot.lua
+++ lua/user/plugins/copilot.lua
@@ -1,3 +1,21 @@
-return{
-  "github/copilot.vim"
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = "<C-f>",
+          -- next = "<C-]>",
+          -- prev = "<C-p>",
+          dismiss = "<C-e>",
+        },
+      },
+      panel = { enabled = false },
+    })
+  end,
 }

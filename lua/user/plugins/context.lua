228046505357f0a000bbfeebeae78aa3d960bb1f
Pradipta Samanta <samanta@dkrz.de>
2026-05-09 10:26:50 +0200

    added context lua

---
+++ lua/user/plugins/context.lua
@@ -0,0 +1,29 @@
+return{
+  "nvim-treesitter/nvim-treesitter-context",
+  lazy = false,
+  config = function()
+    require'treesitter-context'.setup{
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20, -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    }
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>tt", "<cmd>TSContext toggle<cr>",
+      { desc = "Toggle Treesitter-Context" })
+
+    keymap.set("n", "<leader>tc", function()
+      require("treesitter-context").go_to_context(vim.v.count1)
+    end, { silent = true })
+  end,
+}
